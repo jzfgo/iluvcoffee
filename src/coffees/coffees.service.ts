2e0e7f06eaa852d6938eeb45a8a8233a3e927593
Javier Zapata <javierzapata82@gmail.com>
2021-03-22 20:34:03 +0100

    Wrap up "Creating a REST API application" Lesson

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -31,7 +31,6 @@ export class CoffeesService {
   update(id: string, updateCoffeeDto: any) {
     const existingCoffee = this.findOne(id);
     if (existingCoffee) {
-      // update the existing entity
     }
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true, // transform input to its corresponding DTO
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
+
 bootstrap();
