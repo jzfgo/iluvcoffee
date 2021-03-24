85034dc1f929893f683a02646ee82185b77440b8
Javier Zapata <javierzapata82@gmail.com>
2021-03-24 08:01:58 +0100

    Wrap up "Application Configuration" Lesson

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
-    CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: false, // Disable in production
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: process.env.NODE_ENV !== 'production', // Disabled in production
+      }),
     }),
+    ConfigModule.forRoot({
+      load: [appConfig],
+    }),
+    CoffeesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
